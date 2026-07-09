3ba54e0c68cb1b1287377eee07b7d28a87095098
m2构造机 <zhanggaoqi@aobi.com>
2026-07-09 11:06:11 +0800

    [Branch Release] v2.0.3-dev-1481902 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1481902
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "Bugly",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTBuglyHelper/2.0.2-dev-1480335/Bugly.xcframework.zip",
-            checksum: "b848e50839f148276e485aa69b252eed3292f460273b9ef1e791392115f11abe"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTBuglyHelper/2.0.3-dev-1481902/Bugly.xcframework.zip",
+            checksum: "e6d71e36bc6b83378ce5c353aeca40f4a8131c6eece6d5a5c7b630a9890ace73"
         ),
         .binaryTarget(
             name: "UnityBTBuglyHelper",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTBuglyHelper/2.0.2-dev-1480335/UnityBTBuglyHelper.xcframework.zip",
-            checksum: "93776cbb815432c2419efd1f622270fd1101a83258796169943859de70f51adc"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTBuglyHelper/2.0.3-dev-1481902/UnityBTBuglyHelper.xcframework.zip",
+            checksum: "3007c81b010be1cae13913c41f76b9486fd1ef19cfbc38086f14e50f41376764"
         )
     ]
 )
